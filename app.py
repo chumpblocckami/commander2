d25e4d0df52e4a6e5b5998eb2e404547c8190694
chumpblocckami <mazzolauni@gmail.com>
2023-10-10 12:22:14 +0200

    feat: added recent card searched

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 import streamlit as st
 
 from src.checker import Checker
@@ -10,17 +12,39 @@ checker = Checker()
 
 cards = st.text_area(label="Insert here your deck",
                      placeholder="Yotian Soldier\nMyr Retriever\nKrark-Clan Ironworks\nTime Sieve")
-button = st.button(label="Check")
-if button:
-    for card in cards.split("\n"):
-        result = checker.card_check(card.strip())
-        with st.expander(label=f"{result['label'].capitalize()}"):
-            col1, col2 = st.columns([0.7, 0.3])
-            with col1:
-                for title, comment in result["data"].items():
-                    st.markdown(f"**{title.capitalize()}**: {comment}")
-            with col2:
-                for title, resources in result["resources"].items():
-                    st.image(resources, caption=card)
+manual_inserting_button = st.button(label="Check")
+
+if manual_inserting_button:
+    if not cards:
+        st.error("Please enter some cards!")
+    if cards:
+        for card in cards.split("\n"):
+            result = checker.card_check(card.strip())
+            with open("recent_cards.json", "r+") as file:
+                file_data = json.load(file)
+                file_data.append(result)
+                file.seek(0)
+                json.dump(file_data, file)
+            with st.expander(label=f"{result['label'].capitalize()}"):
+                col1, col2 = st.columns([0.7, 0.3])
+                with col1:
+                    for title, comment in result["data"].items():
+                        st.markdown(f"**{title.capitalize()}**: {comment}")
+                with col2:
+                    for title, resources in result["resources"].items():
+                        st.image(resources, caption=card.strip())
+
+st.divider()
+with st.expander(label="Recent searched cards"):
+    with open("recent_cards.json", "r") as file:
+        recent_cards = json.load(file)
+    for recent_card in recent_cards[-10:]:
+        col1, col2 = st.columns([0.7, 0.3])
+        with col1:
+            for title, comment in recent_card["data"].items():
+                st.markdown(f"**{title.capitalize()}**: {comment}")
+        with col2:
+            for title, resources in recent_card["resources"].items():
+                st.image(resources, caption=recent_card["label"].strip())
 
 st.write("by BojukaPod")
